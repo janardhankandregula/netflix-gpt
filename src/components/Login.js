2280259ca7ffffdd1ec5b9d86602acb9ced6c029
janardhankandregula <janardhank134@gmail.com>
2024-08-09 13:12:03 -0400

    success

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import Header from "./Header";
 import { useState } from "react";
 
 const Login = () => {
   const [signIn, setSignIn] = useState(true);
+  const [nameValid, setnameValid] = useState(true);
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setpasswordValid] = useState(true);
+
   const toggleSignIn = () => {
     setSignIn(!signIn);
   };
+
+  const emailRef = useRef(null);
+
+  const passwordRef = useRef(null);
+  const nameRef = useRef(null);
+
+  const validate = (emailRef, passwordRef) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+    const nameRegex = /([a-zA-Z0-9_\s]+)/;
+
+    if (!emailRegex.test(emailRef.current.value)) {
+      setEmailValid(false);
+      //return "Email is not valid";
+    } else {
+      setEmailValid(true);
+    }
+
+    if (!passwordRegex.test(passwordRef.current.value)) {
+      setpasswordValid(false);
+      //return "Password is not valid";
+    } else {
+      setpasswordValid(true);
+    }
+    if (!nameRegex.test(nameRef.current.value)) {
+      setnameValid(false);
+    } else {
+      setnameValid(true);
+    }
+    return null;
+  };
+
+  const handleClick = () => {
+    const validationResult = validate(emailRef, passwordRef);
+    // console.log(result);
+    // if (validationResult) {
+    //   console.log(validationResult); // Output validation error message
+    // }
+    // else {
+    //   console.log("Email and password are valid");
+    // }
+  };
+
   return (
     <div className="relative">
       <Header />
@@ -16,31 +63,61 @@ const Login = () => {
           alt="Des"
         />
       </div>
-      <form className="p-12  w-4/12 absolute top-20 left-1/2 transform -translate-x-1/2 mt-28 bg-black  bg-opacity-85">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="p-12  w-4/12 absolute top-20 left-1/2 transform -translate-x-1/2 mt-28 bg-black  bg-opacity-85 text-white"
+      >
         <div className="flex flex-col">
           <label className="p-2 m-2 text-white font-bold text-4xl">
             {signIn ? "Sign In" : "Sign Up"}
           </label>
-          {signIn ? (
-            ""
-          ) : (
-            <input
-              type="text"
-              placeholder="Full name"
-              className="p-2 m-2 h-14 bg-slate-500 bg-opacity-30 rounded-md"
-            />
+          {signIn ? null : (
+            <>
+              <input
+                ref={nameRef}
+                type="text"
+                placeholder="Full name"
+                className="p-2 m-2 h-14 bg-slate-500 bg-opacity-30 rounded-md"
+              />
+              {!nameValid && (
+                <p className="ml-2 text-sm text-red-600">
+                  Full name is required
+                </p>
+              )}
+            </>
           )}
           <input
+            ref={emailRef}
             type="text"
             placeholder="Email or Mobile number"
             className="p-2 m-2 h-14 bg-slate-500 bg-opacity-30 rounded-md"
           />
+          {/* {isValid ? (
+            ""
+          ) : (
+            <p className="ml-2 text-sm text-red-600">Email is not valid</p>
+          )} */}
+          {!emailValid && (
+            <p className="ml-2 text-sm text-red-600">Email is not valid</p>
+          )}
           <input
+            ref={passwordRef}
             type="password"
             placeholder="password"
             className="p-2 m-2 h-14 bg-slate-500 bg-opacity-30 rounded-md"
           />
-          <button className="px-4 py-2 m-4 bg-red-600 text-white font-bold text-lg rounded-md">
+          {/* {passwordValid ? (
+            ""
+          ) : (
+            <p className="ml-2 text-sm text-red-600">Password is not valid</p>
+          )} */}
+          {!passwordValid && (
+            <p className="ml-2 text-sm text-red-600">Password is not valid</p>
+          )}
+          <button
+            onClick={handleClick}
+            className="px-4 py-2 m-4 bg-red-600 text-white font-bold text-lg rounded-md"
+          >
             {signIn ? "Sign-In" : "Sign-up"}
           </button>
           {signIn ? (
---
+++ src/utilis/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD7CWWPW0e3VMWTnHUGO3UEJY-3ERci_w0",
+  authDomain: "netflix-gpt-6694b.firebaseapp.com",
+  projectId: "netflix-gpt-6694b",
+  storageBucket: "netflix-gpt-6694b.appspot.com",
+  messagingSenderId: "777348770451",
+  appId: "1:777348770451:web:15f56329fa66549a1dfb66",
+  measurementId: "G-F9XZ389LNZ",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
